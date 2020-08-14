51b7b1e06ca164ee4bf98a33949b8d94df9ffc37
Vladyslav Vorobiov <vlad65e@gmail.com>
2020-08-14 17:19:50 +0300

    add ui-expand in app

--- libs/ui/src/lib/components/expand/expand.component.ts
+++ libs/ui/src/lib/components/expand/expand.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-expand',
@@ -7,6 +7,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExpandComponent implements OnInit {
+  @Input() isHidden: boolean;
 
   constructor() { }
 
