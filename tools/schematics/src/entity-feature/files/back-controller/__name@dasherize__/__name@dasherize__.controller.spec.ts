bbd2e68e912f7507e192a37ef42bebbba8300e11
vlad.borsh <vladborsh95s322@gmail.com>
2020-08-15 14:27:52 +0300

    tools: fix scheamtic

--- tools/schematics/src/entity-feature/files/back-controller/__name@dasherize__/__name@dasherize__.controller.spec.ts
+++ tools/schematics/src/entity-feature/files/back-controller/__name@dasherize__/__name@dasherize__.controller.spec.ts
@@ -2,12 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { <%= classify(name) %>Controller } from './<%= dasherize(name) %>.controller';
 import { instance, mock } from 'ts-mockito';
 import { <%= classify(name) %>DomainService } from '../../domain/<%= dasherize(name) %>-domain/<%= dasherize(name) %>-domain.service';
+import { PassportModule } from '@nestjs/passport';
 
 describe('<%= classify(name) %> Controller', () => {
   let controller: <%= classify(name) %>Controller;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
       controllers: [<%= classify(name) %>Controller ],
       providers: [{ provide: <%= classify(name) %>DomainService, useFactory: () => instance(mock(<%= classify(name) %>DomainService))} ],
     }).compile();
