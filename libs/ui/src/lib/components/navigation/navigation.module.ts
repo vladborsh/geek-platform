3a6c808519cbdc1bef5dc204820afd6fbcdd5654
Vladyslav Vorobiov <vlad65e@gmail.com>
2020-08-24 15:25:42 +0300

    add ui desktop only directive

--- libs/ui/src/lib/components/navigation/navigation.module.ts
+++ libs/ui/src/lib/components/navigation/navigation.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation.component';
-import { MobileOnlyModule } from '../mobile-only/mobile-only.module';
+import { MobileOnlyModule } from '../../directives/mobile-only/mobile-only.module';
+import { DesktopOnlyModule } from '../../directives/desktop-only/desktop-only.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, MobileOnlyModule],
+  imports: [CommonModule, RouterModule, MobileOnlyModule, DesktopOnlyModule],
   declarations: [NavigationComponent],
   exports: [NavigationComponent],
 })
