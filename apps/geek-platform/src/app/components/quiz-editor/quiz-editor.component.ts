0250ae122853aaadb7f0474ba64f4e6a01898c14
Vladyslav Vorobiov <vlad65e@gmail.com>
2020-08-27 11:19:08 +0300

    add create new question in quiz editor

--- apps/geek-platform/src/app/components/quiz-editor/quiz-editor.component.ts
+++ apps/geek-platform/src/app/components/quiz-editor/quiz-editor.component.ts
@@ -1,23 +1,38 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { QuizDto, QuestionDto } from '@geek-platform/api-interfaces';
 import { BehaviorSubject } from 'rxjs';
-import { changeQuizName, changeQuestion, removeQuestion } from './quiz-editor.helpers';
+import {
+  changeQuizName,
+  changeQuestion,
+  removeQuestion,
+  generateState,
+  addQuestion,
+} from './quiz-editor.helpers';
 
 export interface State {
-  quiz: QuizDto;
+  isVisibleAddButton: boolean;
+  isVisibleDeleteButton: boolean;
+  quiz: {
+    _id?: string;
+    name: string;
+    questions: Partial<QuestionDto>[];
+  };
 }
 
 @Component({
-    selector: 'app-quiz-editor',
-    templateUrl: './quiz-editor.component.html',
-    styleUrls: ['./quiz-editor.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-quiz-editor',
+  templateUrl: './quiz-editor.component.html',
+  styleUrls: ['./quiz-editor.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuizEditorComponent {
   @Input() set model(quiz: QuizDto) {
-    this.state$.next({ quiz });
+    this.state$.next({
+      ...this.state$.getValue(),
+      quiz: {...quiz},
+    });
   }
-  public state$ = new BehaviorSubject<State>(null);
+  public state$ = new BehaviorSubject<State>(generateState());
 
   public onChangeQuizName(text: string): void {
     this.state$.next(changeQuizName(this.state$.getValue(), text));
@@ -28,14 +43,16 @@ export class QuizEditorComponent {
   }
 
   public onAddQuestion(): void {
+    this.state$.next(addQuestion(this.state$.getValue()));
   }
 
   public onRemoveQuestion(index: number): void {
     this.state$.next(removeQuestion(this.state$.getValue(), index));
   }
 
+  public onSaveQuiz(): void {}
+
   public trackByFunc(i: number): number {
     return i;
   }
-
 }
--- apps/geek-platform/src/app/components/quiz-editor/quiz-editor.helpers.ts
+++ apps/geek-platform/src/app/components/quiz-editor/quiz-editor.helpers.ts
@@ -1,8 +1,42 @@
 import { State } from './quiz-editor.component';
 import { QuestionDto } from '@geek-platform/api-interfaces';
 
+const MIN_COUNT_QUESTIONS = 1;
+const MAX_COUNT_QUESTIONS = 50;
+const DEFAULT_COUNT_QUESTIONS = 3;
+
+function generateQuestion(number: number): Partial<QuestionDto> {
+  return {
+    answers: ['Answer 1', 'Answer 2', 'Answer 3'],
+    actualQuestion: `Question ${number}`,
+    correctAnswer: 0,
+  };
+}
+
+function generateQuestions(): Partial<QuestionDto>[] {
+  const defaultQuestions: Partial<QuestionDto>[] = [];
+
+  for (let i = 0; i < DEFAULT_COUNT_QUESTIONS; i++) {
+    defaultQuestions.push(generateQuestion(i + 1));
+  }
+
+  return defaultQuestions;
+}
+
+export function generateState(): State {
+  return {
+    isVisibleAddButton: true,
+    isVisibleDeleteButton: true,
+    quiz: {
+      name: 'Quiz name',
+      questions: generateQuestions(),
+    },
+  };
+}
+
 export function changeQuizName(state: State, text: string): State {
   return {
+    ...state,
     quiz: {
       ...state.quiz,
       name: text,
@@ -12,9 +46,23 @@ export function changeQuizName(state: State, text: string): State {
 
 export function changeQuestion(state: State, question: QuestionDto, index: number): State {
   return {
+    ...state,
     quiz: {
       ...state.quiz,
       questions: state.quiz.questions.map((item, i) => (i === index ? question : item)),
+    },
+  };
+}
+
+export function addQuestion(state: State): State {
+  const newQuestions = [...state.quiz.questions, generateQuestion(state.quiz.questions.length + 1)];
+
+  return {
+    isVisibleDeleteButton: newQuestions.length > MIN_COUNT_QUESTIONS,
+    isVisibleAddButton: newQuestions.length < MAX_COUNT_QUESTIONS,
+    quiz: {
+      ...state.quiz,
+      questions: newQuestions,
     },
   };
 }
@@ -23,6 +71,8 @@ export function removeQuestion(state: State, index: number): State {
   const newQuestions = state.quiz.questions.filter((_, i) => i !== index);
 
   return {
+    isVisibleDeleteButton: newQuestions.length > MIN_COUNT_QUESTIONS,
+    isVisibleAddButton: newQuestions.length < MAX_COUNT_QUESTIONS,
     quiz: {
       ...state.quiz,
       questions: newQuestions,
