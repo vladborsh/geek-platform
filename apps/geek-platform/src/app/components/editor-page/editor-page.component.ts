9439bfb47adf23e98a8c7c633e99a61245bb5d35
Vladyslav Vorobiov <vlad65e@gmail.com>
2020-09-08 15:38:59 +0300

    add send edited quiz with debounce time

--- apps/geek-platform/src/app/components/editor-page/editor-page.component.ts
+++ apps/geek-platform/src/app/components/editor-page/editor-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { map, switchMap, take, debounceTime, filter, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { QuizDto } from '@geek-platform/api-interfaces';
 import { QuizService } from '../../services/quiz/quiz.service';
 
@@ -11,19 +11,30 @@ import { QuizService } from '../../services/quiz/quiz.service';
   styleUrls: ['./editor-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditorPageComponent implements OnInit {
-  public quiz$: Observable<QuizDto>;
+export class EditorPageComponent implements OnInit, OnDestroy {
+  public quiz$ = new BehaviorSubject<QuizDto>(null);
+  public quizCache$ = new BehaviorSubject<QuizDto>(null);
+  private debounceTime = 300;
+  private onDestroy$ = new Subject<void>();
+
   constructor(private activatedRoute: ActivatedRoute, private quizService: QuizService) {}
 
   ngOnInit(): void {
-    this.quiz$ = this.getQuiz$();
+    this.getQuiz()
+      .subscribe((quiz: QuizDto) => {
+        this.quiz$.next(quiz);
+        this.quizCache$.next(quiz);
+      });
+    this.sendQuiz().subscribe();
   }
 
-  private getQuiz$(): Observable<QuizDto> {
-    return this.getQuizId$()
-      .pipe(
-        switchMap(quizId => this.quizService.getById$(quizId)),
-      );
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
+  public onChangeQuiz(quiz: QuizDto): void {
+    this.quizCache$.next(quiz);
   }
 
   private getQuizId$(): Observable<string> {
@@ -31,6 +42,25 @@ export class EditorPageComponent implements OnInit {
       .params
       .pipe(
         map(params => params.quizId),
+      );
+  }
+
+  private getQuiz(): Observable<unknown> {
+    return this.getQuizId$()
+      .pipe(
+        switchMap(quizId => this.quizService.getById$(quizId)),
+        filter(Boolean),
+        take(1),
+      );
+  }
+
+  private sendQuiz(): Observable<QuizDto> {
+    return this.quizCache$
+      .pipe(
+        filter(quiz => !!quiz && !!quiz._id),
+        debounceTime(this.debounceTime),
+        switchMap(data => this.quizService.update$(data)),
+        takeUntil(this.onDestroy$),
       );
   }
 }
